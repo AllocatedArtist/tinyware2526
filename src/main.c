db4b3088b1285cddf09d7301af0fb99f061c11a4
AllocatedArtist <59222318+AllocatedArtist@users.noreply.github.com>
2026-04-24 22:28:14 -0700

    started working on stack

--- src/main.c
+++ src/main.c
@@ -1,14 +1,50 @@
 #include <raylib.h>
 #include <stdio.h>
+#include <stdlib.h>
+
+#define STACK_DEFAULT_CAPACITY 5
+#define PLAYER_LIVES 3
+
+struct {
+  int playerLives;
+} Globals;
+
+typedef struct {
+  const char *image;
+  int number;
+} Popup;
+
+typedef struct {
+  Popup *data;
+  int headIdx;
+  int size;
+  int capacity;
+} PopupStack;
+
+PopupStack PopupStackCreate() {
+  PopupStack stack;
+  stack.data = malloc(sizeof(PopupStack) * STACK_DEFAULT_CAPACITY);
+  stack.headIdx = -1;
+  stack.size = 0;
+  stack.capacity = STACK_DEFAULT_CAPACITY;
+}
+
+void PopupStackDelete(PopupStack *popupStack) { free(popupStack->data); }
+
+void InitGlobals() { Globals.playerLives = PLAYER_LIVES; }
+
+void UpdateDrawLoop() {
+  BeginDrawing();
+  ClearBackground(RAYWHITE);
+
+  EndDrawing();
+}
 
 int main() {
   InitWindow(1600, 1480, "Window");
   printf("Started Game");
   while (!WindowShouldClose()) {
-    BeginDrawing();
-    ClearBackground(RAYWHITE);
-
-    EndDrawing();
+    UpdateDrawLoop();
   }
   return 0;
 }
